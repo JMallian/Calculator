6e4472e18f1b713f2ed0096e982f5ac83789b0f5
Jessica Mallian <jlmallian@gmail.com>
2018-01-28 15:26:34 -0600

    basically just created another handler function for operators

--- calculator.js
+++ calculator.js
@@ -1,15 +1,34 @@
 window.onload = init;
+var calculations = [];
 
 function init() {
-	var buttons = document.getElementsByClassName("button");
-	for(var i = 0; i < buttons.length; i++) {
-	buttons[i].onclick = handleClick;
+	var numbers = document.getElementsByClassName("num");
+	for(var i = 0; i < numbers.length; i++) {
+	numbers[i].onclick = handleClickForNumbers;
+	}
+
+	var operators = document.getElementsByClassName("operator");
+	for(var i = 0; i < operators.length; i++) {
+		operators[i].onclick = handleClickForOperators;
 	}
 }
 
-function handleClick(e) {
+function handleClickForNumbers(e) {
 	var clickedButton = e.target;
-	var num = clickedButton.getAttribute("id");
 	var display = document.getElementById("display");
-	display.innerHTML = num;
+	display.innerHTML = clickedButton.innerHTML;
+	calculations.push(clickedButton.innerHTML);
+
+	var testDisplay = document.getElementById("test");
+	testDisplay.innerHTML = calculations;
+}
+
+function handleClickForOperators(e) {
+	var clickedButton = e.target;
+	var display = document.getElementById("display");
+	display.innerHTML = clickedButton.innerHTML;
+	calculations.push(clickedButton.innerHTML);
+
+	var testDisplay = document.getElementById("test");
+	testDisplay.innerHTML = calculations;
 }
